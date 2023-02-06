0002979da79b4d3e24b4190a831bb48d38ba0885
Olga <olga-ork@yandex.ru>
2023-02-06 12:50:00 +0300

    Task02

--- Task02.py
+++ Task02.py
@@ -18,6 +18,6 @@ print(list)
 def mult(list):
     mult = []
     for i in range ((len(list)+1)//2):
-     mult.append(list[i]*list[len(list)-1-i])
+        mult.append(list[i]*list[len(list)-1-i])
     return mult
-print(mult(list))
+print(f'Произведение первого и последнего элемента списка = {mult(list)}')
